d50f97f0733ff3b923b9c9db063f847c6fb2cf28
AlexMukha <alexmukha@email.com>
2019-03-04 00:56:20 -0800

    Add concert

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.b913ad853aa241d590be62c90280c0f1,
-  secret: process.env.fda0b62596b44e9880bea22615420db5
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -3,23 +3,45 @@ var spotify = require("node-spotify-api");
 var moment = require("moment");
 var dotenv = require("dotenv");
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 var fs=require("fs");
 var command = process.argv[2];
-var search = process.argv(3)
+var search = process.argv.slice(3).join("-");
+
+function concert(artist){
+    axios.get("https://rest.bandsintown.com/artists/" + artist +  "/events?app_id=codingbootcamp")
+    .then(function(resp){
+       var concerts = resp.data;
+        for (var i=0; i<concerts.length;i++){
+            if (resp.data[i].venue.country === "United States") {
+            var dateTime=resp.data[i].datetime.split("T");
+            var date=moment(dateTime[0],"YYYY-MM-DD").format("MM/DD/YYYY");
+            var time=moment(dateTime[1],"HH:mm:ss").format("LT");
+            console.log("Venue: "+resp.data[i].venue.name);
+            console.log("City: "+resp.data[i].venue.city+", "+resp.data[i].venue.region);
+            console.log("Date: "+date+", @ "+time,"\n============================");
+            // console.log(" ");
+            
+        } 
+    }
+    })
+};
+
 
 if (command === "concert-this") {
-    var findConcert;
-    console.log("Concert");
+    console.log("Concert"+search);
+    concert(search);
 } else if (command === "spotify-this-song") {
-    var spotify;
-    console.log("Song");
+    spotify(search);
+    console.log("Song "+search);
 } else if (command === "movie-this") {
-    var movie;
-    console.log("Movie");
+    movie(search);
+    console.log("Movie "+search);
 } else if (command === "do-what-it-says") {
-    var doit;
-    console.log("Anything");
+    justDoIt(search);
+    console.log("Anything "+search);
+} else {
+    console.log(Error);
 }
 
 
